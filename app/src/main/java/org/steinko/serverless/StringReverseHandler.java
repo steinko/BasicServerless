553bd3af0fc2fd050726fbc45182844be0d68990
steinko <steinkor@gmail.com>
2021-10-04 11:56:19 +0200

    changed package #1

---
+++ app/src/main/java/org/steinko/serverless/StringReverseHandler.java
@@ -0,0 +1,7 @@
+package org.steinko.serverless;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class StringReverseHandler extends SpringBootRequestHandler<String, String> {
+
+}
